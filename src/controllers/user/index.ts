31e1421c3f5f3b37b681b85565ed63a4fc7f7a8b
jlennomatias <john.x12@hotmail.com>
2024-01-13 08:27:47 -0300

    organizando controllers

---
+++ src/controllers/user/index.ts
@@ -0,0 +1,5 @@
+import * as create from "./userController";
+
+export const UserController = {
+  ...create,
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
-import { createUser, deleteUser, getAllUsers, getUserId, updateUser } from "../controllers";
+import { UserController } from "../controllers";
 
 const router = Router();
 
 // Rota para obter todos os dados
-router.get("/users", getAllUsers);
+router.get("/users", UserController.getAllUsers);
 
 // Rota para obter um dado por ID
-router.get("/users/:id", getUserId);
+router.get("/users/:id", UserController.getUserId);
 
 // Rota para criar um novo dado
-router.post("/users", createUser);
+router.post("/users", UserController.createUser);
 
 // Rota para atualizar um dado por ID
-router.put("/users/:id", updateUser);
+router.put("/users/:id", UserController.updateUser);
 
 // Rota para remover um dado por ID
-router.delete("/users/:id", deleteUser);
+router.delete("/users/:id", UserController.deleteUser);
 
 export default router;
